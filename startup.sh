bbbc96beb9916a0a41af2f0a54226c774555cca3
Mitchy <MitchoppDEV@gmail.com>
2025-08-05 21:37:10 +0100

    updated and reduced the sleep timer, also set headphones to 60%

--- startup.sh
+++ startup.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 # startup script
 
-
 notify-send "Welcome back ミッチー 🌸"
-sleep 10
 waypaper --restore
 notify-send "Wallpaper restored."
-sleep 10
+sleep 3
 bluetoothctl connect 00:A4:1C:40:CA:57
 notify-send "Headphones connected."
+
+TARGET_SINK="bluez_output.00_A4_1C_40_CA_57.1"
+for i in {1..15}; do
+    DEFAULT_SINK=$(pactl get-default-sink)
+    if [[ "$DEFAULT_SINK" == "$TARGET_SINK" ]]; then
+        pactl set-sink-volume "$DEFAULT_SINK" 60%
+        notify-send "Volume set to 60%"
+        break
+    fi
+    sleep 1
+done
+
 sleep 5
 ./dotfiles.sh

7bcbd1f5f1bf1e613b9cc442e021be7db8117e9f
Mitchy <MitchoppDEV@gmail.com>
2025-09-25 23:40:05 +0100

    Made it more dramatic

--- git.sh
+++ git.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Please enter a commit message"
-
+echo "--- Enter a commit message: ---"
 read message
+echo "--- Your commit message is: $message ---"
 
 git add .
+echo "'Git add .' Done successfully."
 git commit -m "$message"
+echo "--- Git commit done successfully ---"
 git push origin main
-
-echo "Done."
+echo "--- GIT PUSH DONE! ---"
